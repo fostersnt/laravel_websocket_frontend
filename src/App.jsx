12e26e1a7d2fe1b3d174b710fbe7041ab16ad063
Foster Amponsah <foster.asante@gwosevo.com>
2026-01-15 16:02:40 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { initEcho } from './lib/echo'
 import { login, updateUser } from './lib/api_requests'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [echoInstance, setEchoInstance] = useState(null);
   const [user, setUser] = useState({
     id: 2,
@@ -14,33 +11,20 @@ function App() {
     email: 'fostersnt@gmail.com',
   });
 
-  // const user = {
-  //   id: 2,
-  //   name: 'Foster Amponsah Asante',
-  //   email: 'fostersnt@gmail.com',
-  // };
-
   const logout = () => {
-    // 1. Tell Laravel to revoke the token (optional but secure)
-    // axios.post('/api/logout'); 
-
-    // 2. Clear the token from React
     localStorage.removeItem('AUTH_TOKEN');
-
-    // 3. Disconnect Echo so it stops listening to private channels
     if (echoInstance) {
-        echoInstance.disconnect();
+      echoInstance.disconnect();
     }
-    
-    // 4. Redirect to login
+    //! Ensure the below route is valid inside your react routes
     window.location.href = '/login';
-};
+  };
 
   useEffect(() => {
     async function setup() {
       const apiToken = localStorage.getItem('AUTH_TOKEN');
-      await login(user.email, 'password');  // Login first
-      const echo = initEcho(apiToken);               // Then init Echo
+      await login(user.email, 'password');
+      const echo = initEcho(apiToken);
       setEchoInstance(echo);
 
       await updateUser();
@@ -51,8 +35,6 @@ function App() {
           console.table(JSON.stringify(e.user));
           setUser(e.user);
         });
-
-      // return () => echo.leave(`user.${user.id}`);
     }
 
     setup();
@@ -68,26 +50,6 @@ function App() {
         <h1 className="">WELCOME TO REACT WEBSOCKET</h1>
         <h1 className="">Hi {user.name}</h1>
       </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
